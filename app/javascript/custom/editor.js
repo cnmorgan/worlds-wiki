9d34a58451cabd5547d50f3ca065b82cf5140610
Chris Morgan <cnmorgan@gmail.com>
2020-01-20 23:04:03 -0500

    Fix new pages form

--- app/javascript/custom/editor.js
+++ app/javascript/custom/editor.js
@@ -3,25 +3,61 @@ import 'codemirror/lib/codemirror.css'
 import 'codemirror/mode/markdown/markdown.js'
 
 function initialize(){
-    let textEditor = document.getElementById('page_edit_content')
+    let textArea = document.getElementById('page_edit_content')
     let editor = document.getElementById('content-editor')
-    if(textEditor && !editor){
-        var contentEditor = CodeMirror.fromTextArea(textEditor, {
+
+    if(textArea && !editor){
+        var contentEditor = CodeMirror.fromTextArea(textArea, {
                                                     lineWrapping: true,
                                                     mode: "markdown",
                                                 });
 
         contentEditor.display.wrapper.id = "content-editor"
     }
-    textEditor = document.getElementById('page_edit_summary')
+
+    textArea = null
+    editor = null
+
+    textArea = document.getElementById('page_edit_summary')
     editor = document.getElementById("summary-editor")
-    if(textEditor && !editor){
-        var contentEditor = CodeMirror.fromTextArea(textEditor, {
+
+    if(textArea && !editor){
+        var contentEditor = CodeMirror.fromTextArea(textArea, {
             lineWrapping: true,
             mode: "markdown",
         });
 
         contentEditor.display.wrapper.id = "summary-editor"
+    }
+
+    textArea = null
+    editor = null
+
+    textArea = document.getElementById('page_content')
+    editor = document.getElementById("new-content-editor")
+
+    if(textArea && !editor){
+        var contentEditor = CodeMirror.fromTextArea(textArea, {
+            lineWrapping: true,
+            mode: "markdown",
+        });
+
+        contentEditor.display.wrapper.id = "new-content-editor"
+    }
+
+    textArea = null
+    editor = null
+
+    textArea = document.getElementById('page_summary')
+    editor = document.getElementById("new-summary-editor")
+
+    if(textArea && !editor){
+        var contentEditor = CodeMirror.fromTextArea(textArea, {
+            lineWrapping: true,
+            mode: "markdown",
+        });
+
+        contentEditor.display.wrapper.id = "new-summary-editor"
     }
 }
 
